74f5694ce8a4620e6ae0b34ca54bdcd57c9f40cd
Diego Hurtado <44350813+DiegoHurtad0@users.noreply.github.com>
2020-09-01 15:42:14 -0500

    Add files via upload

---
+++ reverse-string/reverse-string.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            
+            """
+            print(s[left],s[right])
+            print(s[right],s[left])
+            """
+            
+            left, right = left +1, right - 1
+        
